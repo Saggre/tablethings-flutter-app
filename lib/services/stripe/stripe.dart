cebacec4f3974d40669446fb4f3dbb16b51173ca
Saggre <sakri.koskimes@hotmail.com>
2019-12-06 20:23:42 +0200

    Client-side payment logic

--- lib/services/stripe/stripe.dart
+++ lib/services/stripe/stripe.dart
@@ -1,4 +1,50 @@
-/// Stripe API
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:tablething/services/stripe/payment_method.dart';
+
+/// Custom Stripe API
 class Stripe {
-  
+  final String apiUrl = 'https://us-central1-ruokamenu.cloudfunctions.net/api/v1/payment/';
+
+  /// Adds a payment method for a Stripe customer id
+  Future<bool> addPaymentMethod(String customer, PaymentMethod paymentMethod) async {
+    try {
+      http.Response response = await http.post(apiUrl + 'add_payment_method', headers: {
+        'Content-Type': 'application/json'
+      }, body: {
+        "customer": customer,
+        "payment_method": paymentMethod.toJson(),
+      });
+    } catch (err) {
+      throw Exception('Failed to add payment method');
+    }
+
+    return true;
+  }
+
+  /// Gets payment methods attached to a stripe customer id
+  Future<List<PaymentMethod>> getPaymentMethods(String customer, String type) async {
+    List<PaymentMethod> paymentMethods;
+
+    try {
+      http.Response response = await http.post(apiUrl + 'get_payment_methods', headers: {
+        'Content-Type': 'application/json'
+      }, body: {
+        "customer": customer,
+        "type": type,
+      });
+
+      if (response.statusCode == 200) {
+        paymentMethods = (json.decode(response.body)["data"] as List).map((paymentMethod) {
+          return PaymentMethod.fromJson(paymentMethod);
+        }).toList();
+      } else {
+        throw Exception('Failed to get payment methods');
+      }
+    } catch (err) {
+      throw Exception('Failed to get payment methods');
+    }
+
+    return paymentMethods;
+  }
 }
