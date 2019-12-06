ee39a88383b28ca4126eb6a36c2737856aa977dd
Saggre <sakri.koskimes@hotmail.com>
2019-12-06 17:47:40 +0200

    Add Stripe cloud functions

--- cloud/functions/index.js
+++ cloud/functions/index.js
@@ -1,31 +1,126 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const app = require('express')();
+const stripe = require('stripe')(functions.config().stripe.sk);
+const express = require('express');
+const validate = require('express-jsonschema').validate;
+const app = express();
+app.use(express.json());
 
 admin.initializeApp(functions.config().firestore);
 
 const db = admin.firestore();
 const apiVersion = '/v1';
 
-app.get(apiVersion + '/helloWorld', async (req, res) => {
+/**
+ * Gets user object with user id
+ */
+app.get(apiVersion + '/user', validate({
+    body: {
+        type: 'object',
+        properties: {
+            id: {
+                type: 'string',
+                required: true
+            },
+        }
+    },
+}), async (req, res) => {
     res.send("Hello from Firebase!");
 });
 
-app.get(apiVersion + '/user', async (req, res) => {
-    res.send("Hello from Firebase!");
+/**
+ * Creates and adds a payment method for a Stripe user
+ */
+app.post(apiVersion + '/payment/add_payment_method', validate({
+    body: {
+        type: 'object',
+        properties: {
+            customer: {
+                type: 'string',
+                required: true
+            },
+            payment_method: {
+                type: 'object',
+                required: true
+            }
+        }
+    },
+}), async (req, res) => {
+    try {
+        let paymentMethod = await stripe.paymentMethods.create(
+            req.body.payment_method
+        );
+
+        await stripe.paymentMethods.attach(
+            paymentMethod.id,
+            {customer: req.body.customer},
+        );
+
+        res.sendStatus(200);
+
+    } catch (err) {
+        res.status(500).json(getErrorJson(err.message));
+    }
 });
 
-exports.createUserData = functions.auth.user().onCreate((user) => {
-    const userObject = {
+/**
+ * Gets all payment methods for a Stripe customer
+ */
+app.post(apiVersion + '/payment/get_payment_methods', validate({
+    body: {
+        type: 'object',
+        properties: {
+            customer: {
+                type: 'string',
+                required: true
+            },
+            type: {
+                type: 'string',
+                required: true
+            }
+        }
+    },
+}), async (req, res) => {
+    try {
+        let paymentMethods = await stripe.paymentMethods.list(
+            req.body
+        );
+
+        res.status(200).json(paymentMethods);
+
+    } catch (err) {
+        res.status(500).json(getErrorJson(err.message));
+    }
+});
+
+/**
+ * Called on firebase user create
+ * Creates a Stripe customer and adds user data to firestore
+ * @type {CloudFunction<UserRecord>}
+ */
+exports.createUserData = functions.auth.user().onCreate(async (user) => {
+    let userObject = {
         displayName: user.displayName || "",
         email: user.email || "",
+        stripeCustomerId: "",
     };
 
-    db.collection('users').doc(user.uid).set(userObject).catch(error => {
-        console.log('Error writing document: ' + error);
+    try {
+        let customer = await stripe.customers.create({
+            email: user.email,
+        });
+
+        userObject.stripeCustomerId = customer.id;
+    } catch (err) {
+        console.log('Error adding Stripe customer: ' + err);
+    }
+
+    await db.collection('users').doc(user.uid).set(userObject).catch(err => {
+        console.log('Error writing document: ' + err);
         return false;
     });
 });
+
 /*
 exports.user = functions.https.onRequest(async (request, response) => {
     const request_response = await request({
@@ -41,5 +136,16 @@ exports.user = functions.https.onRequest(async (request, response) => {
     console.log('SUCCESS! Posted', snap.ref);
 });
 */
+
+/**
+ * Catch-all error
+ */
+app.get('*', function (req, res) {
+    res.status(404).json(getErrorJson("Error"));
+});
+
+function getErrorJson(errorMsg = "An unknown error occurred") {
+    return {error: true, error_msg: errorMsg};
+}
 
 exports.api = functions.https.onRequest(app);
